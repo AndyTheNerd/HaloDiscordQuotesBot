ba9831d809bdf7c1a07baa34c2468a3dcd15a049
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-06-23 17:11:56 -0400

    Update index and readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# HaloDiscordQuotesBot
-A discord bot that posts quotes from the Halo series!
+# DiscordQuotesBot
+A discord bot that posts quotes at random! Also avaiable as a Twitter bot for Raspberry Pi, located here: https://github.com/AndyTheNerd/HaloTwitterBot 
+
+## Installation
+Disord.js is required to run this and is not included in this repo.
+```bash
+npm install discord.js
+```
+If you plan on running this yourself, you'll also need a Discord API key.
+
+## How to use
+Add the bot to your sever using this invite link: https://discord.com/api/oauth2/authorize?client_id=725038023963705397&permissions=0&scope=bot
+
+Once the bot is in your server, run it by typing in your terminal `node ./index.js`.
+
+The bot command to post a quote at random is `!quote`. 
---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+const config = require('./config.json');
+
+
+client.once('ready', () => {
+	console.log('Ready!');
+	
+	client.user.setActivity("!quote", {
+		type: "STREAMING",
+		//url: "https://github.com/AndyTheNerd/HaloDiscordQuotesBot/"
+	});
+});
+
+client.on('message', async message => {
+	if (message.content === `${config.prefix}quote`) {
+		//-----------------//
+		var quoteList = ["Quote 1, Quote 2, etc..."];
+		var quote = Math.floor(Math.random() * quoteList.length);
+		message.channel.send(quoteList[quote]);
+		//----------------//
+	 } else if (message.content === `${config.prefix}about`) {
+	 	message.channel.send("I'm a discord bot! I was made to post quotes of dialogue from the entire Halo series. I can also be used to post quotes of really anything. To contribute to this project, check out my repo at https://github.com/AndyTheNerd/HaloDiscordQuotesBot!");
+	} 
+});
+
+client.login(config.token);
